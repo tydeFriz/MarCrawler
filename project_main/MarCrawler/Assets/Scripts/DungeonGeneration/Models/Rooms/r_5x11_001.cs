c7d8284397c5a9cbd0527f95cf35dfd78a8951f6
Nicola Valentini <friz3001@gmail.com>
2018-05-08 18:08:35 +0200

    added fourth layout and some rooms

---
+++ project_main/MarCrawler/Assets/Scripts/DungeonGeneration/Models/Rooms/r_5x11_001.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+public class r_5x11_001 : DungeonRoom{
+
+	public r_5x11_001 (bool rotate){
+
+		this.rotate = rotate;
+		treasures = new List<Treasure> ();
+		char[,] defGrid;
+		if (!rotate) {
+			defGrid = new char[5, 11] {
+				{' ', ' ', ' ', 'w', 'w', ' ', 'w', 'w', ' ', ' ', ' '},
+				{'w', 'w', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'w', ' '},
+				{' ', 'w', 'T', ' ', ' ', ' ', ' ', ' ', ' ', 'w', ' '},
+				{' ', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', ' '},
+				{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
+			};
+			treasures.Add (new Treasure (new Coordinates (2, 2)));
+			grid = new DungeonGrid(5, 11, defGrid);
+		} 
+		else {
+			defGrid = new char[11, 5] {
+				{' ', ' ', ' ', 'w', ' '},
+				{' ', 'w', 'w', 'w', ' '},
+				{' ', 'w', 'T', ' ', ' '},
+				{' ', 'w', ' ', ' ', 'w'},
+				{' ', 'w', ' ', ' ', 'w'},
+				{' ', 'w', ' ', ' ', ' '},
+				{' ', 'w', ' ', ' ', 'w'},
+				{' ', 'w', ' ', ' ', 'w'},
+				{' ', 'w', ' ', ' ', ' '},
+				{' ', 'w', 'w', 'w', ' '},
+				{' ', ' ', ' ', ' ', ' '}
+			};
+			treasures.Add (new Treasure (new Coordinates (2, 2)));
+			grid = new DungeonGrid(11, 5, defGrid);
+		}
+		modelFileName = "TEMP_SHITTY_NAME_REMOVE_ME_BITCH";
+
+	}
+
+}
+
---
+++ project_main/MarCrawler/Assets/Scripts/DungeonGeneration/Models/Rooms/r_7x9_001.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+public class r_7x9_001 : DungeonRoom{
+		
+	public r_7x9_001 (bool rotate){
+		
+		this.rotate = rotate;
+		treasures = new List<Treasure> ();
+		char[,] defGrid;
+		if (!rotate) {
+			defGrid = new char[7, 9] {
+				{ 'T', 'w', ' ', ' ', ' ', ' ', 'w', 'w', 'w' },
+				{ ' ', ' ', ' ', 'w', ' ', ' ', 'w', 'w', 'w' },
+				{ 'w', 'w', 'w', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ 'w', 'w', 'w', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', ' ', ' ', 'w', ' ', ' ', 'w', 'w', 'w' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' }
+			};
+			treasures.Add (new Treasure (new Coordinates (0, 0)));
+			grid = new DungeonGrid(7, 9, defGrid);
+		} 
+		else {
+			defGrid = new char[9, 7] {
+				{ ' ', ' ', 'w', ' ', 'w', ' ', 'T' },
+				{ 'w', ' ', 'w', ' ', 'w', ' ', 'w' },
+				{ ' ', ' ', 'w', ' ', 'w', ' ', ' ' },
+				{ ' ', 'w', ' ', ' ', ' ', 'w', ' ' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', ' ', ' ', ' ', ' ', ' ', ' ' },
+				{ ' ', 'w', ' ', ' ', ' ', 'w', 'w' },
+				{ ' ', 'w', ' ', ' ', ' ', 'w', 'w' },
+				{ ' ', 'w', ' ', ' ', ' ', 'w', 'w' }
+			};
+			treasures.Add (new Treasure (new Coordinates (0, 6)));
+			grid = new DungeonGrid(9, 7, defGrid);
+		}
+		modelFileName = "TEMP_SHITTY_NAME_REMOVE_ME_BITCH";
+
+	}
+
+}
+
